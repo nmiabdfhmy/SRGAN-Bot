a6527ed82f7f700903685367933bb73c226c0036
Zelda Projects <56251778+nmiabdfhmy@users.noreply.github.com>
2022-04-21 06:07:52 +0700

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python3 srgan_bot.py
---
+++ config.py
@@ -0,0 +1,17 @@
+# (©)Codexbotz
+# Recode by @UnrealZlda
+# t.me/ZeldaProjects
+
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+
+# Bot token dari @Botfather
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+
+# NAMA OWNER
+OWNER = os.environ.get("OWNER", "UnrealZlda")
+
+# Username CH & Group
+CHANNEL = os.environ.get("CHANNEL", "ZeldaProjects")
+GROUP = os.environ.get("GROUP", "HCMutualism")
